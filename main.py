46ee0f7d80644ab2c39c5e59f5a2a9716eaee4ef
lionking-123 <69095724+lionking-123@users.noreply.github.com>
2020-11-11 15:55:39 +0300

    User Tab Design

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import time
 
 FROM_SPLASH, _ = loadUiType(os.path.join(os.path.dirname(__file__), "./UI/splash.ui"))
 FROM_MAIN, _ = loadUiType(os.path.join(os.path.dirname(__file__), "./UI/main.ui"))
-# FROM_RESET, _ = loadUiType(os.path.join(os.path.dirname(__file__), "reset.ui"))
-# FROM_OPTIONS, _ = loadUiType(os.path.join(os.path.dirname(__file__), "options.ui"))
+FROM_RESET, _ = loadUiType(os.path.join(os.path.dirname(__file__), "./UI/reset.ui"))
+FROM_OPTIONS, _ = loadUiType(os.path.join(os.path.dirname(__file__), "./UI/options.ui"))
 
 # ============= Thread Class ============
 
@@ -31,20 +31,92 @@ class ThreadProgress(QThread) :
             self.mysignal.emit(i)
             i += 1
 
-# ============= Main Window part ==============
+# ============= Main Window Part ==============
 
-class Main(QMainWindow, FROM_MAIN) :
+class Main_window(QMainWindow, FROM_OPTIONS) :
     def __init__(self, parent = None) :
-        super(Main, self).__init__(parent)
+        super(Main_window, self).__init__(parent)
+        QMainWindow.__init__(self)
         self.setupUi(self)
+
+        px = QPixmap("./images/main.png")
+        px = px.scaled(self.size(), Qt.IgnoreAspectRatio)
+        self.back.setPixmap(px)
+
+        self.setWindowIcon(QIcon('./images/icon.png'))
+
+        # btns = (self.users_btn, self.hoem_btn, self.trending_btn, self.categories_btn, self.recommend_btn, self.worldwide_btn, self.reco.btn)
+
+
+    def closeEvent(self, event) :
+        reply = QMessageBox.question(self, 'Window Close', 'Are you sure you want to close the window?',
+                QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes :
+            self.hide()
+            self.dialog = Login(self)
+            self.dialog.show()
+            event.ignore()
+        else :
+            event.ignore()
+
+# ============= Reset Window Part =============
+
+class Reset_window(QMainWindow, FROM_RESET) :
+    def __init__(self, parent = None) :
+        super(Reset_window, self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        
+        px = QPixmap("./images/reset.png")
+        px = px.scaled(self.size(), Qt.IgnoreAspectRatio)
+        self.back.setPixmap(px)
+        
+        self.setWindowIcon(QIcon('./images/icon.png'))
+
+        self.resetLink.clicked.connect(self.resetlink)
+    
+    def resetlink(self) :
+        reply = QMessageBox.question(self, 'Password Reset', 'A password Reset link has been sent to your email',
+                QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+    
+    def closeEvent(self, event) :
+        reply = QMessageBox.question(self, 'Window Close', 'Are you sure you want to close the window?',
+                QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        
+        if reply == QMessageBox.Yes :
+            self.hide()
+            self.dialog = Login(self)
+            self.dialog.show()
+            event.ignore()
+        else :
+            event.ignore()
+    
+# ============= Login Window part ==============
+
+class Login(QMainWindow, FROM_MAIN) :
+    def __init__(self, parent = None) :
+        super(Login, self).__init__(parent)
+        self.setupUi(self)
+
+        px = QPixmap("./images/login.png")
+        px = px.scaled(self.size(), Qt.IgnoreAspectRatio)
+        self.back.setPixmap(px)
+        
+        self.setWindowIcon(QIcon('./images/icon.png'))
+
         self.login_btn.clicked.connect(self.load_main)
         self.reset_btn.clicked.connect(self.load_reset)
     
     def load_main(self) :
-        QMessageBox.about(self, "Alert", "Load Main clicked!")
+        self.hide()
+        self.dialog = Main_window(self)
+        self.dialog.show()
     
     def load_reset(self) :
-        QMessageBox.about(self, "Alert", "Load Reset clicked!")
+        self.hide()
+        self.dialog = Reset_window(self)
+        self.dialog.show()
     
     def keyPressEvent(self, event) :
         if event.key() == Qt.Key_Tab :
@@ -53,6 +125,15 @@ class Main(QMainWindow, FROM_MAIN) :
     def keyPressEvent(self, event) :
         if event.key() == Qt.Key_Enter :
             self.lineEdit.setFocus()
+    
+    def closeEvent(self, event) :
+        reply = QMessageBox.question(self, 'Window Close', 'Are you sure you want to close the application?',
+                QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        
+        if reply == QMessageBox.Yes :
+            self.close()
+        else :
+            event.ignore()
 
 # ============= Splash part ============
 
@@ -62,6 +143,13 @@ class Splash(QMainWindow, FROM_SPLASH) :
         QMainWindow.__init__(self)
         self.setupUi(self)
         
+        px = QPixmap("./images/logo.png")
+        px = px.scaled(self.splash_image.size(), Qt.IgnoreAspectRatio)
+        self.splash_image.setPixmap(px)
+        
+        self.setWindowIcon(QIcon('./images/icon.png'))
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        
         progress = ThreadProgress(self)
         progress.mysignal.connect(self.progress)
         progress.start()
@@ -69,11 +157,11 @@ class Splash(QMainWindow, FROM_SPLASH) :
     @pyqtSlot(int)
     def progress(self, i) :
         self.progressBar.setValue(i)
-        self.progressBar.setFormat("Loading")
+        self.progressBar.setFormat("Loading ...")
         if i == 100 :
-            main = Main(self)
             self.close()
-            main.show()
+            login = Login(self)
+            login.show()
 
 # ============= Application Start Point =============
 
@@ -87,5 +175,5 @@ def main() :
 if __name__ == '__main__' :
     try :
         main()
-    except Execption as why :
+    except Exception as why :
         print(why)
